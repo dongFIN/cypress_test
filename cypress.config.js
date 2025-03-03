5af2e80a3b2fe9dda6f592f86e0679de275e6923
dongFIN <winnie94528@gmail.com>
2025-03-03 23:31:16 +0800

    add example: Record to Test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'vooo1t',
   e2e: {
+    experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
